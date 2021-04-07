872c3477ec0c5944ee5fe839e35eb841dea9334d
Zackory Kelly <zackkelly@live.ca>
2021-04-07 19:39:28 +0000

    Writes data to file and now prints number of bytes saves

--- fetcher.js
+++ fetcher.js
@@ -5,10 +5,18 @@ const URL = process.argv[2];
 const filePath = process.argv[3];
 
 request(URL, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  if (error) {
+    throw error;
+  }
   fs.writeFile(filePath, body, (err) => {
-    if (err) throw err;
-    console.log('File Saved');
+    if (err) {
+      throw err;
+    }
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        throw err;
+      }
+      console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+    });
   });
 });
